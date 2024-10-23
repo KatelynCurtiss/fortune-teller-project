60769e3d7ecdee971992219908af7fbc49fad904
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-23 10:33:54 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+# Katelyn Curtiss
+# October 23 2024
+# Fortune Teller Project
+
+import random 
+import sys 
+import time 
+
+
+fortuneslist = ["You are a winner!", "A secretadmirer will soon send you a sign of affection!", "The one you love is much closer than you think!", "Good things will happen to you by the end of the day today!",  "Fame and fortune will be yours if you take the time to learn Python!", "I'm just a computer program, and have no magical powers!"]
+
+magic_colors = ["blue", "red", "green", "yellow"]
+
+user_name = (input("Enter your first name: "))
+
+print("Welcome to my Python Fortune Teller Program, " + user_name +"!")
+
+question = (input("Do you want me to tell you your fortune? [y/n]: "))
+x = question.lower 
+time.sleep(2)
+
+if question in "y"  "yes": 
+ time.sleep(1)
+  
+
+
+
+
